d93d35ac470f1015904d5b513442121771f0d7a8
smartpeoplethink <spshalperin@gmail.com>
2024-09-06 19:38:52 -0400

    Update kiwiTwinkie.java
    Added testing for servo. Will temporarily break... :/

--- FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/kiwiTwinkie.java
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/kiwiTwinkie.java
@@ -16,9 +16,15 @@ import com.qualcomm.robotcore.hardware.Servo;
 public class kiwiTwinkie extends LinearOpMode{
     private DcMotor leftMotor, rightMotor, frontMotor, flyWheel;
     private double powerLeft = 0, powerRight = 0, powerFront = 0;
-    private boolean runFlyWheel = false, flyWheelWait = false, flyWheelDecrementWait = false, flyWheelIncrementWait = false;
+    private boolean runFlyWheel = false,
+            flyWheelWait = false, flyWheelDecrementWait = false,
+            flyWheelIncrementWait = false, servoReleaseWait = false,
+            servoRelease = true;
     private int flyWheelSpeed = 10;
+    private static double RELEASE = 0, CATCH = 1;
+
     private Vector Movement = new Vector();
+    private Servo servo;
 
     @Override
     public void runOpMode(){
@@ -26,33 +32,49 @@ public class kiwiTwinkie extends LinearOpMode{
         rightMotor = hardwareMap.get(DcMotor.class, "LeftMotor");//Wheel Num 1
         leftMotor = hardwareMap.get(DcMotor.class, "RightMotor");//Wheel Num 2
         frontMotor = hardwareMap.get(DcMotor.class, "FrontMotor");//Wheel Num 0
+        servo = hardwareMap.get(Servo.class, "left_hand");
 
         waitForStart();
         while (opModeIsActive()){
             //rightBumper toggles flywheel
-            if (gamepad1.right_trigger>0.5 && !flyWheelWait){
+            if (gamepad1.left_trigger>0.5 && !flyWheelWait){
                 runFlyWheel = !runFlyWheel;
                 flyWheelWait = true;
             }
-            if (!(gamepad1.right_trigger>0.5)){
+            if (!(gamepad1.left_trigger>0.5)){
                 flyWheelWait = false;
             }
-            if (gamepad1.left_bumper && !flyWheelDecrementWait && flyWheelSpeed > 0){
+            //decrement and increment flywheel
+            if (gamepad1.dpad_down && !flyWheelDecrementWait && flyWheelSpeed > 0){
                 flyWheelSpeed -= 1;
                 flyWheelDecrementWait = true;
             }
-            if (!gamepad1.left_bumper){
+            if (!gamepad1.dpad_down){
                 flyWheelDecrementWait = false;
             }
-            if (gamepad1.right_bumper && !flyWheelIncrementWait && flyWheelSpeed < 10){
+            if (gamepad1.dpad_up && !flyWheelIncrementWait && flyWheelSpeed < 10){
                 flyWheelSpeed += 1;
                 flyWheelIncrementWait = true;
             }
-            if (!gamepad1.right_bumper){
+            if (!gamepad1.dpad_up){
                 flyWheelIncrementWait = false;
             }
 
+            //release mechanism
+            if (gamepad1.right_trigger > 0.5 & !servoReleaseWait){
+                servoRelease = !servoRelease;
+                servoReleaseWait = true;
+            }
+            if (!(gamepad1.right_trigger > 0.5))servoReleaseWait = !servoReleaseWait;
+            //testing servo values:
+            if (gamepad1.right_bumper){
+                RELEASE+=0.01;
+            }
+            if (gamepad1.left_bumper){
+                RELEASE-=0.01;
+            }
 
+            //managing drivint
             //recieve values in a cartesien manner and convert them to polar
             Movement.i = gamepad1.left_stick_x;
             Movement.j = gamepad1.left_stick_y;
@@ -88,6 +110,12 @@ public class kiwiTwinkie extends LinearOpMode{
 
 
             //apply values
+            if (servoRelease){
+                servo.setPosition(RELEASE);
+            }else{
+                servo.setPosition(CATCH);
+            }
+
             if (runFlyWheel){
                 flyWheel.setPower((((double)flyWheelSpeed)/10));
             }else{
@@ -97,7 +125,9 @@ public class kiwiTwinkie extends LinearOpMode{
             rightMotor.setPower(powerRight);
             leftMotor.setPower(powerLeft);
 
-            telemetry.addData("Status",flyWheelSpeed);
+            telemetry.addData("Version of Kiwi: ", 17);
+            telemetry.addData("Speed: ",flyWheelSpeed);
+            telemetry.addData("Servo: ",RELEASE);
             telemetry.update();
         }
     }
